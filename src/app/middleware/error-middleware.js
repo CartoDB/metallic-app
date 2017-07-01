3c8fc356880d2c229eb2556faac1c3b72d301135
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-07-02 01:11:56 +0200

    Improve error middleware

--- src/app/middleware/error-middleware.js
+++ src/app/middleware/error-middleware.js
@@ -1,13 +1,46 @@
 import Middleware from './middleware'
+import { STATUS_CODES } from 'http'
+
+const DEVELOPMENT = 'development'
+const env = process.env.NODE_ENV || DEVELOPMENT
 
 export default class ErrorMiddleware extends Middleware {
   middleware () {
     return async (ctx, next) => {
       try {
         await next()
+
+        if (ctx.status === 404 && !ctx.body) {
+          ctx.throw(404)
+        }
       } catch (err) {
-        ctx.log.error(err)
-        ctx.throw(err) // delegates to koa's default error middleware
+        ctx.status = typeof err.status === 'number' ? err.status : 500
+
+        ctx.log.error(err, `Error ${err.status}: ${err.message}`)
+
+        // application
+        ctx.app.emit('error', err, ctx)
+
+        // accepted types
+        switch (ctx.accepts('text', 'json')) {
+          case 'text':
+            ctx.type = 'text/plain'
+            if (env === DEVELOPMENT || err.expose) {
+              ctx.body = err.message
+            } else {
+              ctx.body = STATUS_CODES[ctx.status]
+            }
+            break
+
+          case 'json':
+            ctx.type = 'application/json'
+            if (env === DEVELOPMENT || err.expose) {
+              ctx.body = { error: err.message }
+            } else {
+              ctx.body = { error: STATUS_CODES[ctx.status] }
+            }
+            break
+        }
       }
     }
   }
