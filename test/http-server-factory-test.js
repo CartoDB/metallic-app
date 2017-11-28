1db77940bec265a4c645356fa8b6d77c7c24d913
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-11-28 20:12:25 +0100

    Use default values to create dependencies

--- test/http-server-factory-test.js
+++ test/http-server-factory-test.js
@@ -1,19 +1,10 @@
 import assert from 'assert'
 import { RunnerInterface } from 'metallic-interfaces'
 import HttpServerFactory from '../src'
-import MetricsFactory from 'metallic-metrics'
 
 describe('http-server-factory', function () {
   it('.create() should return a HttpServer instance', function () {
-    const logger = undefined
-    const metrics = MetricsFactory.create({ logger })
-    const httpServer = HttpServerFactory.create({
-      metrics,
-      logger,
-      options: {
-        port: 3000
-      }
-    })
+    const httpServer = HttpServerFactory.create({})
 
     assert.ok(httpServer instanceof RunnerInterface)
   })
